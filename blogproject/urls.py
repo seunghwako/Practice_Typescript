ebdd79b7a0ede315f0b61e1b2ef3ebd88cb13d67
seunghwako <seunghwa2475@naver.com>
2020-07-12 20:18:03 +0900

    회원가입, login, logout

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', blogapp.views.home, name="home"),
     path('blog/', include('blogapp.urls')),
     path('portfolio/', portfolio.views.portfolio, name="portfolio"),
+    path('accounts/', include('accounts.urls')),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
