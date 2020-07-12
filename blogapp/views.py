7dd6fb7e434faaea40f5b690d2e8a02e18e7d6a0
seunghwako <seunghwa2475@naver.com>
2020-07-12 20:48:30 +0900

    pagination

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.core.paginator import Paginator
 from .models import Blog
 
 # Create your views here.
 def home(request) :
     blogs = Blog.objects
-    return render(request, 'home.html', {'blogs' : blogs})
+    blog_list = Blog.objects.all()
+    paginator = Paginator(blog_list, 3)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    return render(request, 'home.html', {'blogs' : blogs, 'posts' : posts})
 
 def detail(request, blog_id) :
     details = get_object_or_404(Blog, pk=blog_id)
