4cc26171b6b7ab3319735855ef6ae11b6cb86cbf
seunghwako <seunghwa2475@naver.com>
2020-07-14 16:55:33 +0900

    heroku modify

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -140,4 +140,4 @@ MEDIA_URL = '/media/'
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env
+DATABASES['default'].update(db_from_env)
